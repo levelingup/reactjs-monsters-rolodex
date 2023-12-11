71d23217b949aa0678c323b530566d03b3121e50
Andrew Liu <ahliu1@gmail.com>
2023-12-11 10:46:16 -0800

    update react 18

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -4,17 +4,14 @@ import Card from "../card-container/card-container.component";
 
 import "./card-list.styles.css";
 
-const CardList = ({monsters}) => {
-
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return (
-            <Card monster={monster} />
-          )})}
-      </div>
-    );
- }
-
+const CardList = ({ monsters }) => {
+	return (
+		<div className="card-list">
+			{monsters.map((monster) => {
+				return <Card monster={monster} />;
+			})}
+		</div>
+	);
+};
 
 export default CardList;
